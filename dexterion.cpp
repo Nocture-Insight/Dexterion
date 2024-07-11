18d7d49d5cd9ad9f9e856c48fa9baa244340073b
LauraSavall <45919164+LauraSavall@users.noreply.github.com>
2024-07-11 15:29:16 +0200

    Update dexterion.cpp
    Better detection of cs2.exe PID
    Dexterion can now be run BEFORE cs2.exe is opened...
    
    Lot of people don't read...

--- dexterion.cpp
+++ dexterion.cpp
@@ -9,6 +9,9 @@
 #include "util/MemMan.hpp"
 #include "util/attributes.hpp"
 
+#include <iostream>
+#include <psapi.h> // For EnumProcesses
+
 LRESULT Wndproc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) {
 		return 0;
@@ -28,6 +31,25 @@ LRESULT Wndproc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
+// Function to get the PID of a process by its name
+DWORD GetPIDByName(const wchar_t* processName) {
+	DWORD pid = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32W pe32; // Using PROCESSENTRY32W for wide strings (wchar_t)
+		pe32.dwSize = sizeof(PROCESSENTRY32W);
+		if (Process32FirstW(hSnap, &pe32)) { // Using Process32FirstW for wide strings (wchar_t)
+			do {
+				if (_wcsicmp(pe32.szExeFile, processName) == 0) { // Using _wcsicmp for wide strings (wchar_t) comparison
+					pid = pe32.th32ProcessID;
+					break;
+				}
+			} while (Process32NextW(hSnap, &pe32)); // Using Process32NextW for wide strings (wchar_t)
+		}
+		CloseHandle(hSnap);
+	}
+	return pid;
+}
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 	AllocConsole();
@@ -41,6 +63,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	system("cmd.exe /C updateoffset.cmd");
 	SetConsoleTextAttribute(hConsole, 10);
 	printf("[MemMan] Required files updated\n");
+
+	DWORD cs2Pid = 0;
+
+	while (cs2Pid == 0) {
+		// Search for cs2.exe among the PIDs
+		cs2Pid = GetPIDByName(L"cs2.exe");
+		if (cs2Pid != 0) {
+			SetConsoleTextAttribute(hConsole, 10);
+			std::wcout << L"cs2.exe found! PID: " << cs2Pid << std::endl;
+		}
+		else {
+			SetConsoleTextAttribute(hConsole, 12);
+			std::wcout << L"cs2.exe not found. Retrying in 5 seconds..." << std::endl;
+			Sleep(5000); // Wait for 5 seconds before the next attempt
+		}
+	}
 	
 	// Memory and game related vars (used in entry and passed through overlay)
 	int procId = MemMan.getPid(L"cs2.exe");
