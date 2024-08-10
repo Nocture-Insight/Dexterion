44c5156efb540165d2e83a0ab3fc27da68d1bbc1
kvfs <sepixcraft48@gmail.com>
2024-08-10 04:14:52 +0200

    Path required to find fileName

--- json/jsonOps.hpp
+++ json/jsonOps.hpp
@@ -3,14 +3,12 @@
 #include <fstream>
 
 #include "json.hpp"
-
 #include "../util/utilFunctions.hpp"
 
 namespace json {
 	std::wstring clientDLLFile = L"\\client.dll.json";
-	std::wstring configFile = L"\\config.json";
 	std::wstring offsetFile = L"\\offsets.json";
 	std::wstring buttonsFile = L"\\buttons.json";
 
-	nlohmann::json readFromJsonFile(std::wstring fileName);
+	nlohmann::json readFromJsonFile(std::wstring path, std::wstring fileName);
 }
