640c86bec91a7048658371884cd98267bce96ed9
kvfs <sepixcraft48@gmail.com>
2024-08-14 20:02:18 +0200

    Update version

--- util/utilFunctions.hpp
+++ util/utilFunctions.hpp
@@ -83,7 +83,7 @@ inline namespace Logger {
 }
 
 inline namespace utils {
-	inline std::string version = "2.2.0-Hotfix";
+	inline std::string version = "2.2.1";
 
 	// https://www.unknowncheats.me/forum/dayz-sa/129893-calculate-distance-meters.html
 	// https://www.unknowncheats.me/forum/general-programming-and-reversing/478087-calculate-size-esp-boxes-based-distance.html
